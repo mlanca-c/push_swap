46e60b2f2312d33a2d2e224a7bdf5c0f610b2def
MLSTT <marialsottomayor@gmail.com>
2021-05-19 18:09:38 +0100

    Update push_swap.h status     - updatted commentary - KO

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 13:29:29 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/05/18 20:17:16 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/05/19 17:34:41 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "libft.h"
 
-void	fill_stack(char **argv, t_stack *stack_a);
+void	fill_stack(char **argv, t_stack **stack_a);
 void	exit_program(t_stack *stack_a, t_stack *stack_b, int status);
 
 #endif
