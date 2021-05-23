53f7b9e5b542599f57e6e25bd4723f0d87f346d8
mlanca-c <mlanca-c@student.42lisboa.com>
2021-05-23 23:52:54 +0100

    Update push_swap.c         status:         -Memory Leaks     - OK         -Error Management - OK         -Checker (Bonus)  - KO         -Identity Test    - OK         -Visualizer       - KO         -Instructions     - OK         -Simple Version   - OK         -Medium Version   - OK         -Advance Version  - KO

--- sources/push_swap/sorting_utils.c
+++ sources/push_swap/sorting_utils.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 22:48:36 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/05/23 23:19:44 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/05/23 23:52:24 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /*
  * This function pushes the two integers smallest integers from stack_a to
- * stack_b
+ * stack_b.
 */
 void	push_min_to_b(t_stack **stack_a, t_stack **stack_b)
 {
@@ -22,6 +22,11 @@ void	push_min_to_b(t_stack **stack_a, t_stack **stack_b)
 
 	minimum = ft_stack_min_value(*stack_a);
 	while ((*stack_a)->data != minimum)
-		rotate_stack(stack_a, 0, "ra\n");
+	{
+		if ((*stack_a)->data != minimum && (*stack_a)->next->data != minimum)
+			reverse_rotate_stack(stack_a, 0, "rra\n");
+		else
+			rotate_stack(stack_a, 0, "ra\n");
+	}
 	push_stack(stack_a, stack_b, "pb\n");
 }
