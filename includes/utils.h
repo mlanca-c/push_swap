297be0047769d04295b9a0dab7c606eb7df4f954
mlanca-c <mlanca-c@student.42lisboa.com>
2021-05-25 18:40:32 +0100

    Update utils.h         status:         -Memory Leaks     - OK         -Error Management - OK         -Checker (Bonus)  - OK         -Identity Test    - OK         -Visualizer       - KO         -Instructions     - OK         -Simple Version   - OK         -Medium Version   - OK         -Advance Version  - KO

---
+++ includes/utils.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/24 19:12:39 by mlanca-c          #+#    #+#             */
+/*   Updated: 2021/05/25 17:20:32 by mlanca-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+# include "libft.h"
+
+/*
+** push_swap_utils.c Functions
+*/
+void	move_to_stack(char **argv, t_stack **stack_a);
+void	exit_push_swap(t_stack *stack_a, t_stack *stack_b, int status);
+void	push_min_to_b(t_stack **stack_a, t_stack **stack_b);
+
+/*
+** instructions.c Functions
+*/
+void	swap_stack(t_stack *mandatory, t_stack *optional, char *message);
+void	push_stack(t_stack **from_stack, t_stack **to_stack, char *message);
+void	rotate_stack(t_stack **mandatory, t_stack **optional, char *message);
+void	reverse_rotate_stack(t_stack **mandatory,
+			t_stack **optional,
+			char *message);
+
+#endif
