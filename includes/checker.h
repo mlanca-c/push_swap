83f4d574fb01cf9c7a893def7b6f3d08431cc3c0
mlanca-c <mlanca-c@student.42lisboa.com>
2021-05-25 18:39:59 +0100

    Update checker.h         status:         -Memory Leaks     - OK         -Error Management - OK         -Checker (Bonus)  - OK         -Identity Test    - OK         -Visualizer       - KO         -Instructions     - OK         -Simple Version   - OK         -Medium Version   - OK         -Advance Version  - KO

--- includes/checker.h
+++ includes/checker.h
@@ -6,11 +6,30 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 23:38:08 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/05/23 23:38:37 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/05/25 18:25:23 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECKER_H
 # define CHECKER_H
+
+# include "libft.h"
+# include "get_next_line.h"
+# include "utils.h"
+#include <stdio.h>
+
+/*
+** checker.c Functions
+*/
+void	get_instructions(t_stack **stack_a, t_list **instructions);
+int		instruction_is_valid(char *line);
+void	implement_instructions(t_stack **stack_a, t_stack **stack_b,
+			t_list *instructions);
+
+/*
+** checker_utils.c Functions
+*/
+void	exit_checker(t_stack *stack_a, t_stack *stack_b, t_list *instructions,
+			int status);
 
 #endif
