a3a32f624389923a7abdd979fd27c2268ae56451
mlanca-c <mlanca-c@student.42lisboa.com>
2021-05-25 18:40:16 +0100

    Update push_swap.h         status:         -Memory Leaks     - OK         -Error Management - OK         -Checker (Bonus)  - OK         -Identity Test    - OK         -Visualizer       - KO         -Instructions     - OK         -Simple Version   - OK         -Medium Version   - OK         -Advance Version  - KO

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 20:50:25 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/05/23 23:04:15 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/05/25 17:19:59 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,17 @@
 # define PUSH_SWAP_H
 
 # include "libft.h"
+# include "utils.h"
 #include <stdio.h>
 
 /*
 ** push_swap.c Functions
 */
-void	move_to_stack(char **argv, t_stack **stack_a);
-void	exit_push_swap(t_stack *stack_a, t_stack *stack_b, int status);
-
-/*
-** instructions.c Functions
-*/
-void	swap_stack(t_stack *mandatory, t_stack *optional, char *message);
-void	push_stack(t_stack **from_stack, t_stack **to_stack, char *message);
-void	rotate_stack(t_stack **mandatory, t_stack **optional, char *message);
-void	reverse_rotate_stack(t_stack **mandatory,
-			t_stack **optional,
-			char *message);
-
-/*
-** sorting.c Functions
-*/
 void	get_sorting(t_stack **stack_a, t_stack **stack_b);
 void	sorting_small_algorithm(t_stack **stack_a);
 void	sorting_medium_algorithm(t_stack **stack_a, t_stack **stack_b);
 void	sorting_big_algorithm(t_stack **stack_a, t_stack **stack_b);
+
 
 /*
 ** sorting_utils.c Functions
