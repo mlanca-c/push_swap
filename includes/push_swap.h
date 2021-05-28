a023698ff339f0ff2bbf5452ff7f09a5abd434ff
MLSTT <marialsottomayor@gmail.com>
2021-05-28 11:06:57 +0100

    Update push_swap.h status: -Memory Leaks     - ✅ -Error Management - ✅ -Checker (Bonus)  - ✅ -Identity Test    - ✅ -Visualizer       - ✅ -Instructions     - ✅ -Simple Version   - ✅ -Medium Version   - ✅ -Advance Version  - 🦾

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 20:50:25 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/05/27 14:40:09 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/05/28 10:37:53 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,9 @@ void	get_chunks(t_stack **stack_a, t_stack **chunks);
 int		get_hold_first(t_stack *stack_a, t_stack *chunks);
 int		get_hold_second(t_stack *stack_a, t_stack *chunks);
 void	split_a_to_b(t_stack **stack_a, t_stack **stack_b, t_stack **chunks);
+
+/*
+** push_swap_utils1.c Functions
+*/
 
 #endif
