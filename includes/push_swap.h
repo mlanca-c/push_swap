1cc873498ad46db2e7b02dda78ff431ae77412cc
MLSTT <marialsottomayor@gmail.com>
2021-06-02 19:23:42 +0100

    Update push_swap.h         status:         -Memory Leaks     - ✅         -Error Management - ✅         -Checker (Bonus)  - ✅         -Identity Test    - ✅         -Visualizer       - ✅         -Instructions     - ✅         -Simple Version   - ✅         -Medium Version   - ✅         -Advance Version  - 🦾

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 20:50:25 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/06/01 17:02:47 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/06/02 19:17:34 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,18 @@ void	sorting_big_algorithm(t_stack **stack_a, t_stack **stack_b,
 ** push_swap_utils.c Functions
 */
 void	push_min_to_b(t_stack **stack_a, t_stack **stack_b);
-void	get_chunks(t_stack **stack_a, t_stack **chunks);
-int		get_hold_first(t_stack *stack_a, t_stack *chunks);
-int		get_hold_second(t_stack *stack_a, t_stack *chunks);
-void	split_a_to_b(t_stack **stack_a, t_stack **stack_b, t_stack **chunks);
+void	get_new_limit(t_stack **limits, t_stack *stack_a);
+void	split_a_to_b(t_stack **stack_a, t_stack **stack_b, t_stack *limits);
+int		get_hold_first(t_stack *stack_a, t_stack *limits);
+int		get_hold_second(t_stack *stack_a, t_stack *limits);
 
 /*
 ** push_swap_utils1.c Functions
 */
-void	merge_half_to_a(t_stack **stack_a, t_stack **stack_b, t_stack **chunks);
-void	split_b_ra(t_stack **stack_b, t_stack **stack_a, int first);
-void	split_b_rra(t_stack **stack_b, t_stack **stack_a, int second);
+void	get_holds(int *first, int *second, t_stack *stack_a, t_stack *limits);
+void	merge_half_to_a(t_stack **stack_a, t_stack **stack_b, t_stack *limits);
 void	merge_sort_to_a(t_stack **stack_a, t_stack **stack_b);
-int		ra_closest(t_stack *stack_b, int value);
+void	rotate_until_sorted(t_stack **stack_a, t_stack *limits);
+int		count_in_between(t_stack *stack_a, t_stack *limits);
 
 #endif
