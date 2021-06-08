650797cd83ec8ea1220c0f0522bb9b73fc427de1
mlanca-c <mlanca-c@student.42lisboa.com>
2021-06-08 18:25:32 +0100

    Update push_swap_utils1.c             status:             -Memory Leaks     - ✅             -Error Management - ✅             -Checker (Bonus)  - ✅             -Identity Test    - ✅             -Visualizer       - ✅             -Instructions     - ✅             -Simple Version   - ✅             -Medium Version   - ✅

--- sources/push_swap_utils1.c
+++ sources/push_swap_utils1.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 10:36:15 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/06/02 19:17:05 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/06/08 18:20:53 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,32 +42,35 @@ void	get_holds(int *first, int *second, t_stack *stack_a, t_stack *limits)
 }
 
 /*
+ * This function takes a stack - 'stack_b' - and all its numbers above the
+ * second element of 'limits' are pushed back to 'stack_a'
+ * 		example:
+ * 			50 random numbers between 1 and 50;
+ * 			stack_a: ]25, 50] unsorted
+ * 			stack_b: [1, 25] unsorted
+ * 			limits: {1, 13, 25, 50}
+ * 		all numbers bigger than 13 have to go back to stack_a.
 */
 void	merge_half_to_a(t_stack **stack_a, t_stack **stack_b, t_stack *limits)
 {
-	int	first;
-	int	second;
-	int	size;
-
 	get_new_limit(&limits, *stack_b);
-	size = count_in_between(*stack_b, limits->next) - 1;
-	while (ft_stack_size(*stack_b) > size)
+	while (ft_stack_has_bigger(*stack_b, limits->next->data))
 	{
-		first = get_hold_first(*stack_b, limits->next);
-		second = get_hold_second(*stack_b, limits->next);
-		if (first <= second)
-			while (first--)
-				rotate_stack(stack_b, 0, "rb\n");
+		if ((*stack_b)->data == ft_stack_min_value(*stack_b))
+		{
+			push_stack(stack_b, stack_a, "pa\n");
+			rotate_stack(stack_a, 0, "ra\n");
+		}
+		else if ((*stack_b)->data > limits->next->data)
+			push_stack(stack_b, stack_a, "pa\n");
 		else
-			while (second--)
-				reverse_rotate_stack(stack_b, 0, "rrb\n");
-		push_stack(stack_b, stack_a, "pa\n");
+			rotate_stack(stack_b, 0, "rb\n");
 	}
 }
 
 /*
 */
-void	merge_sort_to_a(t_stack **stack_a, t_stack **stack_b)
+void	merge_sort_to_a(t_stack **stack_a, t_stack **stack_b, t_stack *limits)
 {
 	t_stack	*first;
 	t_stack	*second;
@@ -91,7 +94,9 @@ void	merge_sort_to_a(t_stack **stack_a, t_stack **stack_b)
 		else
 			rotate_stack(stack_b, 0, "rb\n");
 	}
-
+	while (ft_stack_last(*stack_a)->data != limits->next->data)
+		rotate_stack(stack_a, 0, "ra\n");
+	limits->next->data = get_next_value(*stack_a, &limits);
 }
 
 /*
@@ -115,4 +120,23 @@ int	count_in_between(t_stack *stack_a, t_stack *limits)
 	min_idx = ft_stack_find(duplicate, limits->data);
 	max_idx = ft_stack_find(duplicate, limits->next->data);
 	return (max_idx - min_idx + 1);
+}
+
+/*
+*/
+int	get_next_value(t_stack *stack_a, t_stack **limits)
+{
+	t_stack	*duplicate;
+	int		position;
+	int		minimum;
+
+	duplicate = ft_stack_duplicate(stack_a);
+	ft_stack_sort(&duplicate);
+	position = ft_stack_find(duplicate, (*limits)->next->data);
+	minimum = ft_stack_get(duplicate, position);
+	if (minimum == ft_stack_last(duplicate)->data)
+		return (minimum);
+	else
+		minimum = ft_stack_get(duplicate, position + 1);
+	return (minimum);
 }
