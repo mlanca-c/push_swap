7601ee02ac710445e3899462192207dd626111da
mlanca-c <mlanca-c@student.42lisboa.com>
2021-06-08 18:24:24 +0100

    Update libft

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 20:50:25 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/06/02 19:17:34 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/06/08 18:07:59 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ void	get_sorting(t_stack **stack_a, t_stack **stack_b);
 void	sorting_small_algorithm(t_stack **stack_a);
 void	sorting_medium_algorithm(t_stack **stack_a, t_stack **stack_b);
 void	sorting_big_algorithm(t_stack **stack_a, t_stack **stack_b,
-		t_stack **chunks);
+		t_stack **limits, int i);
 
 /*
 ** push_swap_utils.c Functions
 */
 void	push_min_to_b(t_stack **stack_a, t_stack **stack_b);
 void	get_new_limit(t_stack **limits, t_stack *stack_a);
-void	split_a_to_b(t_stack **stack_a, t_stack **stack_b, t_stack *limits);
+int		split_a_to_b(t_stack **stack_a, t_stack **stack_b, t_stack *limits);
 int		get_hold_first(t_stack *stack_a, t_stack *limits);
 int		get_hold_second(t_stack *stack_a, t_stack *limits);
 
@@ -39,8 +39,9 @@ int		get_hold_second(t_stack *stack_a, t_stack *limits);
 */
 void	get_holds(int *first, int *second, t_stack *stack_a, t_stack *limits);
 void	merge_half_to_a(t_stack **stack_a, t_stack **stack_b, t_stack *limits);
-void	merge_sort_to_a(t_stack **stack_a, t_stack **stack_b);
+void	merge_sort_to_a(t_stack **stack_a, t_stack **stack_b, t_stack *limits);
 void	rotate_until_sorted(t_stack **stack_a, t_stack *limits);
 int		count_in_between(t_stack *stack_a, t_stack *limits);
+int		get_next_value(t_stack *stack_a, t_stack **limits);
 
 #endif
